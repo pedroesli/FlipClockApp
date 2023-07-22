4b552b4b04c3f9fdfe0692b7d42180d6fc9cafa8
Pedro Esli <pedroesli.vn@gmail.com>
2023-07-21 22:43:28 -0300

    feat: first push

---
+++ FlipClock/FlipClock/FlipClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipClockApp.swift
+//  FlipClock
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FlipClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
