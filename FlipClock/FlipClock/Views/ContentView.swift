77cdb13fba7a400f656640448c1a5899697f62c2
Pedro Ésli <>
2023-08-04 15:44:38 -0300

    refactor: better content view animation

--- FlipClock/FlipClock/Views/ContentView.swift
+++ FlipClock/FlipClock/Views/ContentView.swift
@@ -13,40 +13,36 @@ struct ContentView: View {
     @State private var showSettingsView = false
     @State private var selectedTabOption: TabOption = .clock
     @State private var showTabBar = false
-    @State private var navigationBarHeight: CGFloat = .zero
-    
-    var showToolBar: Visibility {
-        showTabBar ? .visible : .hidden
-    }
     
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.asset.background.ignoresSafeArea()
-                VStack {
-                    ClockView(showTabBar: $showTabBar)
-                        .navigationBarTitleDisplayMode(.inline)
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarTrailing) {
+                GeometryReader { geometry in
+                    VStack {
+                        Color.clear
+                            .frame(height: showTabBar ? geometry.safeAreaInsets.top : 0)
+                            .overlay(alignment: .bottomTrailing) {
                                 Button {
                                     showSettingsView = true
                                 } label: {
                                     Image(systemName: "gearshape.fill")
                                         .foregroundColor(settingsManager.appColor)
+                                        .font(.title2)
+                                        .padding(.vertical, 9)
+                                        .padding(.horizontal, 16)
                                 }
                             }
-                        }
-                        .toolbar(showToolBar, for: .navigationBar)
-                        .navigationDestination(isPresented: $showSettingsView) {
-                            ConfigurationView()
-                        }
+                            .offset(y: showTabBar ? 0 : -geometry.safeAreaInsets.top)
+                        ClockView(showTabBar: $showTabBar)
+                        RoundTabBar(selectedTabOption: $selectedTabOption)
+                            .offset(y: showTabBar ? 0 : geometry.safeAreaInsets.bottom * 2)
+                            .frame(height: showTabBar ? nil : 0)
+                    }
                 }
-                VStack(spacing: 0) {
-                    RoundTabBar(selectedTabOption: $selectedTabOption)
-                        .offset(y: showTabBar ? 0 : 100)
-                        .frame(height: showTabBar ? nil : 0)
-                }
-                .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+            .navigationDestination(isPresented: $showSettingsView) {
+                ConfigurationView()
             }
         }
         .tint(settingsManager.appColor)
